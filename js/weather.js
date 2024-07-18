49e2644e6e8de3daa43f7bdc94779c2e63da4ae0
ElifAkin11 <165787747+ElifAkin11@users.noreply.github.com>
2024-07-18 23:28:11 +0300

    sonhali

---
+++ js/weather.js
@@ -0,0 +1,37 @@
+async function fetchCity() {
+    let city;
+
+const url = "http://api.weatherapi.com/v1/current.json?key=ca7685f4ad1240139bd154225240607&q=Ankara&aqi=no";
+
+await fetch(url)
+    .then(response =>{
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("error");
+        }
+    })
+    .then(data => {
+        country = {
+            location: data.location.name + ", Turkey ",
+            temperature: data.current.temp_c + "°C " 
+        }
+        return country;
+    })
+    
+    
+    const temperature = document.querySelector("#temperature"); 
+    const ankara = document.querySelector("#country");
+
+    temperature.innerHTML=country.temperature;
+    ankara.innerHTML=country.location;
+
+} 
+
+fetchCity();
+
+
+
+
+
+    
